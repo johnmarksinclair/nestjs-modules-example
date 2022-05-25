91a49278be787e743bee8884085c6a32a404f2ca
John Sinclair <sinclairjohn72@gmail.com>
2022-05-25 13:10:39 +0100

    added second 'customers' module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CustomersModule } from './modules/customers/customers.module';
 import { EmployeesModule } from './modules/employees/employees.module';
 
 @Module({
-  imports: [EmployeesModule],
+  imports: [EmployeesModule, CustomersModule],
   controllers: [AppController],
   providers: [AppService],
 })
